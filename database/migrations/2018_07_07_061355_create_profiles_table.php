a8115fdacd6680cd0f22276799e93cdba9892c35
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2018-07-07 14:26:00 +0700

    #1 Database design

---
+++ database/migrations/2018_07_07_061355_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->date('born_date');
+            $table->enum('gender', ['L', 'P'])->nullable();
+            $table->string('number_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
---
+++ database/migrations/2018_07_07_070936_create_classes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('course_id')->unsigned();
+            $table->integer('lecture_id')->unsigned();
+            $table->integer('periode_id')->unsigned();
+            $table->integer('day')->unsigned();
+            $table->time('start');
+            $table->time('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
